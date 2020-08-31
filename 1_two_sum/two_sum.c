36e1afdb6ad699dfea02ecd1be69bea8e7681ef2
tangjianxiong@allwinnertech.com <tangjianxiong@allwinnertech.com>
2020-08-31 22:49:13 +0800

    add two sum v0.1

--- 1_two_sum/two_sum.c
+++ 1_two_sum/two_sum.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
 int *twoSum(int *nums, int numsSize, int target, int *returnSize)
 {
+    static int res[2] = {0};
+    int temp = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        temp = nums[i];
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[j] == target - temp)
+            {
+                res[0] = i;
+                res[1] = j;
+                *returnSize = 2;
+                return res;
+            }
+        }
+    }
+    return 0;
 }
